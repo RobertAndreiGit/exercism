8a495fb2ae80e2aa749e2554eca3484160217a3c
Robert Amarandei <robert_amarandei@yahoo.com>
2021-09-06 21:10:50 +0300

    Update word-count solution

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,12 +1,6 @@
 export default class Words{
     count(str: string): Map<string, number>{
-        const words = str.trim().toLowerCase().split(/\s+/);
-
-        let result: Map<string, number> = new Map();
-        for(let word of words){
-            result.set(word, result.get(word)! + 1 || 1);
-        }
-
-        return result;
+        return str.trim().toLowerCase().split(/\s+/).reduce((acc, word) => 
+            acc.set(word,acc.get(word)! + 1 || 1), new Map<string, number>());
     }
 }
