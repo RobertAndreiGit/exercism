9a7696473490f85302e50046f0f63e3916aad414
Robert Amarandei <robert_amarandei@yahoo.com>
2021-09-23 21:33:10 +0300

    Update resistor-color-duo solution

--- typescript/completed/resistor-color-duo/resistor-color-duo.ts
+++ typescript/completed/resistor-color-duo/resistor-color-duo.ts
@@ -1,6 +1,6 @@
-type COLOR = 'black' | 'brown' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'violet' | 'grey' | 'white'; 
-const RESISTORS = new Map<COLOR, number>([['black', 0], ['brown', 1], ['red', 2], ['orange', 3], ['yellow', 4], ['green', 5], ['blue', 6], ['violet', 7], ['grey', 8], ['white', 9]]);
+type COLOR = 'black' | 'brown' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'violet' | 'grey' | 'white';
+const COLORS: COLOR[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export function decodedValue(arr: Readonly<COLOR[]>): number {
-  return Number(`${RESISTORS.get(arr[0])}${RESISTORS.get(arr[1])}`);
+  return COLORS.indexOf(arr[0])*10 + COLORS.indexOf(arr[1]);
 }
