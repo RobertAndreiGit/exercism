7826d4f30be92b8c88a31c91134735730c803d89
Robert Amarandei <robert_amarandei@yahoo.com>
2021-09-23 21:49:46 +0300

    Update resistor-color-duo solution

--- typescript/completed/resistor-color-duo/resistor-color-duo.ts
+++ typescript/completed/resistor-color-duo/resistor-color-duo.ts
@@ -1,6 +1,6 @@
-type COLOR = 'black' | 'brown' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'violet' | 'grey' | 'white';
-const COLORS: COLOR[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'] as const;
+type COLOR = typeof COLORS;
 
-export function decodedValue(arr: Readonly<COLOR[]>): number {
+export function decodedValue(arr: Readonly<COLOR>): number {
   return COLORS.indexOf(arr[0])*10 + COLORS.indexOf(arr[1]);
 }
